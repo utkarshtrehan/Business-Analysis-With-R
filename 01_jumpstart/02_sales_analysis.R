1d4c18016ab22a29ab37a07922136027a19ac976
unknown <utkarshtrehan@icloud.com>
2021-02-16 14:22:11 +0100

    Part 1

--- 01_jumpstart/02_sales_analysis.R
+++ 01_jumpstart/02_sales_analysis.R
@@ -1,5 +1,5 @@
 # DS4B 101-R: R FOR BUSINESS ANALYSIS ----
-# JUMPSTART: First Sales Analysis----
+# JUMPSTART: First Sales Analysis ----
 
 # 1.0 Load libraries ----
 
@@ -19,23 +19,45 @@ library(writexl)
 
 # 2.0 Importing Files ----
 
+?read_excel()
 
+bikes_tbl <- read_excel(path = "00_data/bike_sales/data_raw/bikes.xlsx")
+
+bikeshops_tbl <- read_excel("00_data/bike_sales/data_raw/bikeshops.xlsx")
+
+orderlines_tbl <- read_excel("00_data/bike_sales/data_raw/orderlines.xlsx")
 
 
 
 # 3.0 Examining Data ----
 
+bikes_tbl
 
+glimpse(bikes_tbl)
 
+bikeshops_tbl
 
+orderlines_tbl
 
 # 4.0 Joining Data ----
 
+?left_join
 
+orderlines_tbl
 
+bikes_tbl
+
+left_join(orderlines_tbl, bikes_tbl, by = c("product.id" = "bike.id"))
+
+bike_orderlines_joined_tbl <- orderlines_tbl %>%
+    left_join(bikes_tbl, by = c("product.id" = "bike.id")) %>%
+    left_join(bikeshops_tbl, by = c("customer.id" = "bikeshop.id"))
+
+bike_orderlines_joined_tbl
+
+bike_orderlines_joined_tbl %>% glimpse()
 
 # 5.0 Wrangling Data ----
-
 
 
 
