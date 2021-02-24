d16d5342dea683f74e6d3dea94fc31ebc9fe44b3
unknown <utkarshtrehan@icloud.com>
2021-02-24 23:02:50 +0100

    Text Analysis:Feature Engineering Case Study

--- 02_data_wrangling/04_text.R
+++ 02_data_wrangling/04_text.R
@@ -133,3 +133,62 @@ bike_orderlines_colnames_tbl %>%
 # 2.0 Feature Engineering with Text -----
 # Investigating "model" and extracting well-formatted features
 
+bikes_tbl %>%
+    
+    select(model) %>%
+    
+    # Fix typo
+    mutate(model = case_when(
+        model == "CAAD Disc Ultegra" ~ "CAAD12 Disc Ultegra",
+        model == "Syapse Carbon Tiagra" ~ "Synapse Carbon Tiagra",
+        model == "Supersix Evo Hi-Mod Utegra" ~ "Supersix Evo Hi-Mod Ultegra",
+        TRUE ~ model
+    )) %>%
+    
+    # separate using spaces
+    separate(col     = model, 
+             into    = str_c("model_", 1:7), 
+             sep     = " ", 
+             remove  = FALSE, 
+             fill    = "right") %>%
+    
+    # creating a "base" feature
+    mutate(model_base = case_when(
+        
+        # Fix Supersix Evo
+        str_detect(str_to_lower(model_1), "supersix") ~ str_c(model_1, model_2, sep = " "),
+        
+        # Fix Fat CAAD bikes
+        str_detect(str_to_lower(model_1), "fat") ~ str_c(model_1, model_2, sep = " "),
+        
+        # Fix Beast of the East
+        str_detect(str_to_lower(model_1), "beast") ~ str_c(model_1, model_2, model_3, model_4, sep = " "),
+        
+        # Fix Bad Habit
+        str_detect(str_to_lower(model_1), "bad") ~ str_c(model_1, model_2, sep = " "),
+        
+        # Fix Scalpel 29
+        str_detect(str_to_lower(model_2), "29") ~ str_c(model_1, model_2, sep = " "),
+        
+        # catch all
+        TRUE ~ model_1)
+    ) %>%
+    
+    # Get "tier" feature
+    mutate(model_tier = model %>% str_replace(model_base, replacement = "") %>% str_trim()) %>%
+    
+    # Remove unnecessary columns
+    select(-matches("[0-9]")) %>%
+    
+    # Create Flags
+    mutate(
+        black     = model_tier %>% str_to_lower() %>% str_detect("black") %>% as.numeric(),
+        hi_mod    = model_tier %>% str_to_lower() %>% str_detect("hi-mod") %>% as.numeric(),
+        team      = model_tier %>% str_to_lower() %>% str_detect("team") %>% as.numeric(),
+        red       = model_tier %>% str_to_lower() %>% str_detect("red") %>% as.numeric(),
+        ultegra   = model_tier %>% str_to_lower() %>% str_detect("ultegra") %>% as.numeric(),
+        dura_ace  = model_tier %>% str_to_lower() %>% str_detect("dura ace") %>% as.numeric(),
+        disc      = model_tier %>% str_to_lower() %>% str_detect("disc") %>% as.numeric()
+    ) %>%
+    
+    View()
